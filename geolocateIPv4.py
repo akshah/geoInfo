c20254a464dfcd751d6aeac9259a56a3bcc98624
Anant Shah <akshah@cs.colostate.edu>
2016-03-06 18:11:26 -0700

    New path for latest maxmind bin

--- geolocateIPv4.py
+++ geolocateIPv4.py
@@ -6,8 +6,8 @@ if len(sys.argv) != 2:
     print('Give me an IP to geolocate.')
 host=sys.argv[1]
 
-#maxmind = MaxMindRepo('/home3/akshah/Playground/dataEngine/mm/20160105_maxmind_bin')
-maxmind = MaxMindRepo()
+maxmind = MaxMindRepo('/home3/akshah/akshah_cron_bin/latest_maxmind_bin')
+#maxmind = MaxMindRepo()
 print('Using bin file: '+maxmind.binFileLocation)
 #print(maxmind.ipToCountry(str(host)))
 dict=eval(maxmind.ipLocation(str(host)))
