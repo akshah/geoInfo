555198509582d9cc43c04529496ae07be3b769f2
Anant Shah <akshah@cs.colostate.edu>
2017-01-21 14:50:13 -0700

    Added flask code

--- sample/geolocateIP.py
+++ sample/geolocateIP.py
@@ -1,15 +1,25 @@
 from __future__ import print_function
 from geoInfo.MaxMindRepo import MaxMindRepo
 import sys
+import traceback
+#if len(sys.argv) != 2:
+#   print('Give me an IP to geolocate.')
 
-if len(sys.argv) != 2:
-    print('Give me an IP to geolocate.')
-host=sys.argv[1]
 
 maxmind = MaxMindRepo('/home3/akshah/akshah_cron_bin/latest_maxmind_bin')
 #maxmind = MaxMindRepo()
 print('Using bin file: '+maxmind.binFileLocation)
-#print(maxmind.ipToCountry(str(host)))
-dict=eval(maxmind.ipLocation(str(host)))
-for key in dict:
-    print(key,': ',dict[key])
+print('host|country_code|region_code|city|latitude|longitude')
+for lR in sys.stdin:
+    try:
+        line=lR.rstrip('\n').rstrip('\r')
+        host=line
+        #print(maxmind.ipToCountry(str(host)))
+        dict=eval(maxmind.ipLocation(str(host)))
+        #for key in dict:
+        #    print(key,': ',dict[key])
+        print(str(host)+'|'+str(dict['country_code'])+'|'+str(dict['region_code'])+'|'+ \
+              str(dict['city'])+'|'+str(dict['latitude'])+'|'+str(dict['longitude']))
+    except:
+        print(str(host))
+        traceback.print_exc()
